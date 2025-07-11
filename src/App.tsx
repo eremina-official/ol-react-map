d9490e344a6ed86e988a4f326698bd68011351e9
Marina Eremina <eremina.jobmail@gmail.com>
2025-07-11 14:59:37 +0200

    Edit description

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,28 @@ import Map from './Map';
 
 function App() {
   return (
-    <div>
-      <Map />
+    <div className="main">
+      <div className="content">
+        <h1>Simple Map</h1>
+        <p>
+          This is a simple responsive map built using <b>OpenLayers</b> and <b>React</b>. No API key
+          required for this map.
+        </p>
+        <p>
+          The map can be easily customized and integrated into your application. Whether you're
+          building a dashboard, integrating a map into your webpage, or just experimenting with
+          mapping tools, this guide provides a clean and practical starting point.
+        </p>
+        <p>
+          The map includes essential features such as <b>zoom controls</b>, a custom{' '}
+          <b>attribution</b>, and a <b>custom layer to display a specific location</b> on the map.
+        </p>
+        <p>
+          The map is centered on London, UK, and includes a custom pin icon. You can change the
+          coordinates and the icon to suit your needs.
+        </p>
+        <Map />
+      </div>
     </div>
   );
 }
